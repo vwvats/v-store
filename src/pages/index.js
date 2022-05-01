c8076ebf3f83deff5c34444f038da92aef7afa13
VW Vats <vwvats@gmail.com>
2022-05-01 23:51:44 +0530

    modified the query for products to get images from cloudinary

--- src/pages/index.js
+++ src/pages/index.js
@@ -104,11 +104,7 @@ export async function getStaticProps() {
           name
           price
           slug
-          image {
-            url
-            width
-            height
-          }
+          image
         }
       }
     `
